046f4e0fcb683fd31af74d6d4d43fd5ca2eb9f96
Rudi Giesler <rudi@praekelt.org>
2019-03-15 16:53:04 +0200

    We need a default secret key for generating static files

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ COPY . /app
 RUN pip install --no-cache-dir -e . 
 
 ENV DJANGO_SETTINGS_MODULE "nurseconnect_registration.settings"
-RUN SECREY_KEY=foo ./manage.py collectstatic --noinput
+RUN ./manage.py collectstatic --noinput
 CMD ["nurseconnect_registration.wsgi:application"]
--- nurseconnect_registration/settings.py
+++ nurseconnect_registration/settings.py
@@ -25,7 +25,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", str, "REPLACEME")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", bool, False)
