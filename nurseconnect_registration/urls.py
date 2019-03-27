f6ec59cb674288ea9371d8b6e003da88767940e3
OMosimege <onalerona.mosimege@gmail.com>
2019-03-27 14:56:41 +0200

    ran isort -rc sorting

--- nurseconnect_registration/urls.py
+++ nurseconnect_registration/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django_prometheus.exports import ExportToDjangoView as metrics
-from nurseconnect_registration.views import TermsAndConditionsView
 
 from nurseconnect_registration.decorators import internal_only
+from nurseconnect_registration.views import TermsAndConditionsView
 
 urlpatterns = [
     path("", include(("registrations.urls", "registrations"))),
