34cf80da43722d197f4fb53343a9077949ee5e7b
Kaitlyn Crawford <kaitlyn@praekelt.com>
2019-04-09 12:57:56 +0200

    Cache the contact for 10min after retrieving it

--- nurseconnect_registration/testsettings.py
+++ nurseconnect_registration/testsettings.py
@@ -6,3 +6,10 @@ DATABASES = {"default": env.db(default="sqlite:///:memory")}  # noqa F405
 
 RAPIDPRO_URL = "https://test.rapidpro"
 RAPIDPRO_TOKEN = "some_token"
+
+CACHES = {
+    'contacts': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'contacts',
+    }
+}
--- registrations/utils.py
+++ registrations/utils.py
@@ -1,8 +1,17 @@
 from django.conf import settings
+from django.core.cache import caches
 from temba_client.v2 import TembaClient
 
 
+CACHE = caches["contacts"]
+CACHE_EXPIRY_SECONDS = 600
+
+
 def get_contact(msisdn):
-    client = TembaClient(settings.RAPIDPRO_URL, settings.RAPIDPRO_TOKEN)
-    contact = client.get_contacts(urn='tel:%s' % msisdn).first()
+    contact = CACHE.get(msisdn)
+    if not contact:
+        client = TembaClient(settings.RAPIDPRO_URL, settings.RAPIDPRO_TOKEN)
+        contact = client.get_contacts(urn='tel:%s' % msisdn).first()
+        CACHE.set(msisdn, contact, CACHE_EXPIRY_SECONDS)
+
     return contact
