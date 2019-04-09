2f3386bc655670bc1e0930c851d37bf311585152
Kaitlyn Crawford <kaitlyn@praekelt.com>
2019-04-09 17:25:35 +0200

    Redirect to optin confirmation page if they've previously opted out

--- registrations/urls.py
+++ registrations/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 
 from registrations.api_views import ReferralLinkViewSet
 from registrations.views import (
+    RegistrationConfirmOptIn,
     RegistrationConfirmClinic,
     RegistrationDetailsView,
     RegistrationSuccess,
@@ -14,6 +15,7 @@ api_router.register("referral_link", ReferralLinkViewSet)
 
 urlpatterns = [
     path("", RegistrationDetailsView.as_view(), name="registration-details"),
+    path("confirm_optin", RegistrationConfirmOptIn.as_view(), name="confirm-optin"),
     path("confirm_clinic", RegistrationConfirmClinic.as_view(), name="confirm-clinic"),
     path("success", RegistrationSuccess.as_view(), name="success"),
     path(
--- registrations/views.py
+++ registrations/views.py
@@ -5,6 +5,7 @@ from django.views.generic.edit import FormView
 
 from registrations.forms import RegistrationDetailsForm
 from registrations.models import ReferralLink
+from registrations.utils import contact_in_groups
 
 
 class RegistrationDetailsView(FormView):
@@ -26,12 +27,36 @@ class RegistrationDetailsView(FormView):
         self.request.session["registration_details"] = form.cleaned_data
         # TODO: Replace with result of clinic code check
         self.request.session["clinic_name"] = "Test clinic"
+
+        if contact_in_groups(form.cleaned_data['msisdn'], ['opted-out']):
+            return redirect(reverse_lazy("registrations:confirm-optin"))
         return super().form_valid(form)
 
     def get_initial(self):
         if "registration_details" in self.request.session:
             return self.request.session["registration_details"]
         return super().get_initial()
+
+
+class RegistrationConfirmOptIn(TemplateView):
+    template_name = "registrations/confirm_optin.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if "registration_details" not in request.session or 'msisdn' not in \
+                request.session.get('registration_details', {}):
+            return redirect(reverse_lazy("registrations:registration-details"))
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["msisdn"] = self.request.session["registration_details"]["msisdn"]
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if "yes" in request.POST:
+            request.session["channel"] = "WhatsApp"
+            return redirect(reverse_lazy("registrations:confirm-clinic"))
+        return redirect(reverse_lazy("registrations:registration-details"))
 
 
 class RegistrationConfirmClinic(TemplateView):
